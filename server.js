63fff6b17071286e040810855e63b23ac0055915
Mike Matcke <mr.matcke@gmail.com>
2021-05-13 17:56:14 -0700

    updating files trying to debug

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const compression = require("compression")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -17,6 +22,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 });
 
 
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
