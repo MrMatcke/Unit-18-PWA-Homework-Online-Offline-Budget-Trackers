6d826cc20cee51a3cda6ac86587ec7f0e18f8991
Mike Matcke <mr.matcke@gmail.com>
2021-05-13 20:12:42 -0700

    trying to troubleshoot

--- public/db.js
+++ public/db.js
@@ -1,6 +1,4 @@
-
 let db;
-// create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
@@ -50,5 +48,4 @@ function checkDatabase() {
   };
 }
 
-// listen for app coming back online
 window.addEventListener("online", checkDatabase);
