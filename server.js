eeccfef4027f963a3455a15e0014b5d45ba79783
Mike Matcke <mr.matcke@gmail.com>
2021-05-16 14:42:47 -0700

    troubleshooting heroku issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression")
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,14 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
-
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
